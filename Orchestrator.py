1f962a1d43edf35977467dc7628d0c0c21c8ca7f
puccinellirpr <puccinellirpr@gmail.com>
2019-04-25 02:58:44 -0300

    Verificação do termo de busca no retorno da API e retorno da abbrev para futura pesquisa de textos Versão 2.0

--- Orchestrator.py
+++ Orchestrator.py
@@ -3,8 +3,9 @@ import json
 
 
 def main():
-    search_mecanism()
-
+    search = input('O que deseja pesquisar?')
+    search_list = search.split(':') and search.split()
+    search_mecanism(book_request(), search_list[0])
 
 def book_request():
     json_r = requests.get('https://bibleapi.co/api/books/')
@@ -12,15 +13,14 @@ def book_request():
     return result
 
 
-def search_mecanism():
-    search = input('O que deseja pesquisar?')
-    search_list = search.split(':') and search.split()
-    nome = search_list[0]
-    lista = book_request()
-    for i in lista:
-        if nome == i.get('name'):
-            abbrev= i.get('abbrev')
-            return abbrev
-
+def search_mecanism(list,search_for):
+    search_at = 0
+    search_res = False
+    while search_at < len(list) and search_res == False:
+        if search_for in list[search_at].get('name'):
+            search_res = list[search_at].get('abbrev')
+            return search_res
+        else:
+           search_at = search_at + 1
 
 main()
